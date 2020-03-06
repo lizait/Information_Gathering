6e00d2a25cda42edfb53367ebf227c697b9ade93
Krystian8 <whitestarr888@gmail.com>
2020-03-06 01:03:44 +0100

    Version 0.2

--- Nmap_Auto.py
+++ Nmap_Auto.py
@@ -1,12 +1,9 @@
+import sys
 import nmap
-import nmap3
-from nmap3.nmapparser import NmapCommandParser
-import shlex
-import subprocess
-from xml.etree import ElementTree as ET
+import nessrest
 
 
-def getOpenPorts(hosts, ports=None, arguments="-sV", sudo=False):
+def getOpenPorts(hosts, ports=None, arguments="-sV -sC", sudo=False):
     """
     Scan by nmap given hosts and their ports
 
@@ -15,58 +12,51 @@ def getOpenPorts(hosts, ports=None, arguments="-sV", sudo=False):
     :param arguments: string of arguments for nmap
     :param sudo: true if launch by sudo
 
-    :return: scan result as dictionary
+    :returns: scan result as dictionary
+    :error: can raise AttributeError if bad arguments and nmap.PortScannerError for all problem in nmap
     """
 
     scanner = nmap.PortScanner()
-    listHosts = scanner.scan(hosts=hosts, ports=ports, arguments=arguments, sudo=sudo)
+
+    try:
+        nmapResult = scanner.scan(hosts=hosts, ports=ports, arguments=arguments, sudo=sudo)
+    except nmap.PortScannerError:
+        print("PortScannerError exception ==> problem with nmap: {}".format(nmap.PortScannerError.args))
+        return -1
 
     result_scan = {}
 
-    for host, info in listHosts["scan"].items():
-        result_scan[host] = {}
-        result_scan[host]["name"] = info["hostnames"][0]["name"]
-        result_scan[host]["tcp"] = {}
-        if "tcp" in info:
-            for p, port_info in info["tcp"].items():
-                result_scan[host]["tcp"][p] = {}
-                result_scan[host]["tcp"][p]["state"] = port_info["state"]
-                result_scan[host]["tcp"][p]["service"] = port_info["name"]
-                result_scan[host]["tcp"][p]["version"] = port_info["product"] + port_info["version"]
-                result_scan[host]["tcp"][p]["extrainfo"] = port_info["extrainfo"]
+    if "error" in nmapResult["nmap"]["scaninfo"]:
+        for error in nmapResult["nmap"]["scaninfo"]["error"]:
+            print(error)
+        raise AttributeError()
+    else:
+        for host, info in nmapResult["scan"].items():
+            result_scan[host] = {}
+            result_scan[host]["name"] = info["hostnames"][0]["name"]
+            result_scan[host]["tcp"] = {}
+            if "tcp" in info:
+                for p, port_info in info["tcp"].items():
+                    result_scan[host]["tcp"][p] = {}
+                    result_scan[host]["tcp"][p]["state"] = port_info["state"]
+                    result_scan[host]["tcp"][p]["service"] = port_info["name"]
+                    result_scan[host]["tcp"][p]["version"] = port_info["product"] + port_info["version"]
+                    result_scan[host]["tcp"][p]["extrainfo"] = port_info["extrainfo"]
 
     return result_scan
 
 
-
 if __name__ == '__main__':
 
-    resultScan = getOpenPorts("192.168.137.86", arguments="-sn", ports="-p-")
+    try:
+        resultScan = getOpenPorts("192.168.137.3/24", arguments="-sn")
+        resultScan = getOpenPorts("192.168.137.3/24")
+
+    except:
+        t = sys.exc_info()
+        print("Unexpected error:", sys.exc_info()[0])
     print("next")
 
-
-    print("temp")
-
-
-
-    '''nm.command_line()  # get command line used for the scan : nmap -oX - -p 22-443 127.0.0.1
-    nm.scaninfo()  # get nmap scan informations {'tcp': {'services': '22-443', 'method': 'connect'}}
-    nm.all_hosts()  # get all hosts that were scanned
-    nm['127.0.0.1'].hostname()  # get one hostname for host 127.0.0.1, usualy the user record
-    nm['127.0.0.1'].hostnames()  # get list of hostnames for host 127.0.0.1 as a list of dict
-    # [{'name':'hostname1', 'type':'PTR'}, {'name':'hostname2', 'type':'user'}]
-    nm['127.0.0.1'].hostname()  # get hostname for host 127.0.0.1
-    nm['127.0.0.1'].state()  # get state of host 127.0.0.1 (up|down|unknown|skipped)
-    nm['127.0.0.1'].all_protocols()  # get all scanned protocols ['tcp', 'udp'] in (ip|tcp|udp|sctp)
-    nm['127.0.0.1']['tcp'].keys()  # get all ports for tcp protocol
-    nm['127.0.0.1'].all_tcp()  # get all ports for tcp protocol (sorted version)
-    nm['127.0.0.1'].all_udp()  # get all ports for udp protocol (sorted version)
-    nm['127.0.0.1'].all_ip()  # get all ports for ip protocol (sorted version)
-    nm['127.0.0.1'].all_sctp()  # get all ports for sctp protocol (sorted version)
-    nm['127.0.0.1'].has_tcp(22)  # is there any information for port 22/tcp on host 127.0.0.1
-    nm['127.0.0.1']['tcp'][22]  # get infos about port 22 in tcp on host 127.0.0.1
-    nm['127.0.0.1'].tcp(22)  # get infos about port 22 in tcp on host 127.0.0.1
-    nm['127.0.0.1']['tcp'][22]['state']'''
 
 
 
